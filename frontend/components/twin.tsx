18bbed9edf83109d2cbf53a4a4bb3e8902f44dcb
tuchan2002 <trananhtu171002@gmail.com>
2025-11-07 16:50:00 +0700

    add terraform

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('https://4yi9l9od03.execute-api.ap-southeast-1.amazonaws.com/chat', {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/chat`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
